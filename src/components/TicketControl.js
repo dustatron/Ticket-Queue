da61e47ed1c32ae17a905f89dc3b84fddb83ccd1
dustatron <dustymccord@gmail.com>
2020-04-28 21:34:19 -0700

    test update.

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -7,6 +7,7 @@ import EditTicketForm from "./EditTicketForm";
 import { connect } from "react-redux";
 import * as a from './../actions';
 
+
 class TicketControl extends React.Component {
   constructor(props) {
     super(props);
